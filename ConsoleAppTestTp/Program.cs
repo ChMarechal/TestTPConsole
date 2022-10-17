d1d6d8051c502ef60480e4409be3ef05dfa5332d
Christian Maréchal <c.marechal@thomas-piron.eu>
2022-10-17 15:39:07 +0200

    New Ajouter des lignes dans Programme Répondre à Erik

--- ConsoleAppTestTp/Program.cs
+++ ConsoleAppTestTp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleAppTestTp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Christian!");
+            Console.ReadKey();
+            Console.WriteLine("Bonjour Erik")
         }
     }
 }
